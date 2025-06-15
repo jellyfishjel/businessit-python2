e76b89a5cf85a51eb6c92d9de5c9c0eea6534b6d
jellyfishjel <meimeicute06@gmail.com>
2025-06-15 22:42:14 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+# ========== Áp dụng font và CSS toàn cục ==========
+def apply_global_styles():
+    st.markdown("""
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+        <style>
+            * {
+                font-family: 'Inter', sans-serif;
+            }
+
+            /* Sidebar font and color */
+            section[data-testid="stSidebar"] *,
+            section[data-testid="stSidebar"] {
+                font-family: 'Inter', sans-serif !important;
+                color: #333 !important;
+            }
+
+            /* Gender multiselect tags */
+            [data-baseweb="tag"] {
+                background-color: #ffd7b5 !important;
+                color: white !important;
+            }
+
+            /* Phần hiển thị đã chọn */
+            div[data-baseweb="select"] > div {
+                background-color: #FFF3E0 !important;
+                border: 2px solid #cf5a2e !important;
+                color: #333 !important;
+            }
+
+            [data-testid="stMetricLabel"] {
+                color: #333 !important;
+            }
+
+            label, p {
+                color: #333 !important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
